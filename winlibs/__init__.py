645dc8d06e5e669ea44cb0883742957e8f0170cc
kiln707 <steven.guy.swanson@gmail.com>
2019-02-04 15:50:00 -0800

    fixes to allow updates to working environment

--- winlibs/__init__.py
+++ winlibs/__init__.py
@@ -1,6 +1,6 @@
-from ADQuery import*
-from User import *
-from pyad import set_defaults
+from .ADQuery import *
+from .User import *
+from pyad import pyad
 
 def init(ldap_server, username, password):
     pyad.set_defaults(ldap_server=ldap_server, username=username, password=password)
