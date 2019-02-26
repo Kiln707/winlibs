417cfa77dddb1f6e39e1564eea45a898bb5d9ca2
Kiln707 <steven.guy.swanson@gmail.com>
2019-02-25 16:21:57 -0800

    misc changes

--- research_hold.py
+++ research_hold.py
@@ -1,4 +1,11 @@
 from winlibs.adsi.NTObjects import *
+from winlibs.adsi.Base import ADSIBaseObject
 
+computer = NTComputer('sswanson')
 user = NTUser('sswanson')
-print(user.FullName)
+for i in computer:
+    j = ADSIBaseObject(adsi_com_object=i)
+    for m in dir(user):
+        if m not in dir(j):
+            print(m)
+    break
