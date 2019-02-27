274100abdc25a42249aed42ac0c050bd40fab2f0
Kiln707 <steven.guy.swanson@gmail.com>
2019-02-26 16:27:56 -0800

    misc

--- research_hold.py
+++ research_hold.py
@@ -1,11 +1,22 @@
 from winlibs.adsi.NTObjects import *
 from winlibs.adsi.Base import ADSIBaseObject
 
-computer = NTComputer('sswanson', options={'server':None})
-user = NTUser('sswanson', options={'server':None})
-for i in computer:
-    j = ADSIBaseObject(adsi_com_object=i)
-    for m in j.get_attributes():
-        if m not in user.get_attributes():
-            print(m)
-    break
+computer = NTComputer('sswanson')
+user = NTUser('sswanson')
+group = NTGroup('Administrators', options={'server':None})
+domain=NTDomain(options={'server':'NTNET'})
+service = NTService('Spooler', options={'server':None})
+
+# for obj in computer:
+for obj in domain:
+    # print(obj.Name)
+    #ntobj = NTObject(adsi_com_object=obj)
+    #ntobj = NTObject.get_object(obj)
+    print(obj.Name)
+    print(obj)
+
+    # if obj._class == 'Service':
+    #     for o in obj.get_attributes():
+    #         print(o, getattr(obj, o))
+    #     break
+    # print(ntobj.get_attributes())
