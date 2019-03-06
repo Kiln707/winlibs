068ac3a463cd2499ce6b600b34817c2a60418d43
Kiln707 <steven.guy.swanson@gmail.com>
2019-03-05 16:24:38 -0800

    misc

--- research_hold.py
+++ research_hold.py
@@ -5,5 +5,18 @@ p = PowerCFG()
 a=0
 for s in p.list():
     for i in s.settings():
-        print(i._GUID, i._name)
+        pass
+#        print(i._GUID, i._name)
     break
+
+from pyad import pyad
+
+from winlibs.adsi.LDAPObjects import *
+from winlibs.adsi.NTObjects import *
+
+u = LDAPBaseObject('CN=John Doe,OU=Accounts,DC=solano,DC=cc,DC=ca,DC=us')
+
+
+print(u.get('distinguishedName'))
+
+print(u)
