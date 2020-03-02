976a345299bb04b91b0b25ae926b2d2456018b2f
kiln707 <steven.guy.swanson@gmail.com>
2020-03-01 20:41:46 -0800

    starting os

--- winlibs/internals/OperatingSystem.py
+++ winlibs/internals/OperatingSystem.py
@@ -1,6 +1,27 @@
 from .wmic import WMIRoot
 
-class OperatingSystemWMI(WMIRoot):
+class OperatingSystem(WMIRoot):
      def __init__(self, host='localhost', username='', password='', find_classes=False):
          super().__init__(self, host=host, username=username, password=password, find_classes=find_classes)
-         
+         self.bootDevice = ''
+         self.buildNumber = ''
+         self.buildType = ''
+         self.caption = ''
+         self.codeSet = ''
+         self.countryCode = ''
+         self.creationClassName = ''
+         self.csCreationClassName = ''
+         self.csdVersion = ''
+         self.csdVersion = ''
+
+    def _setInformation(self):
+        pass
+
+    def reboot(self):
+        pass
+
+    def setDateTime(self):
+        pass
+
+    def shutdown(self):
+        pass
